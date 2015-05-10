f7f85ea386f2bcae675c71247f4c6e644bccf6bd
tcandens <joey.thies@gmail.com>
2015-05-10 00:08:08 -0700

    Working tests and router

--- test/api_test.js
+++ test/api_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 process.env.MONGOLAB_URI = 'mongodb://localhost/emperors_test';
-// process.env.PORT = 3333;
+var port = process.env.PORT = 3333;
 require('../lib/server');
 
 var Emperor = require('../lib/models/emperor');
@@ -20,7 +20,7 @@ describe('Emperors API', function() {
   });
 
   it('Should send greeting with info on API location', function(done) {
-    chai.request('localhost:3000')
+    chai.request('localhost:' + port)
       .get('/')
       .end(function(err, res) {
         expect(res.body.message).to.equal('Welcome, our API of Roman Emperors housed at the /api URL.');
@@ -29,7 +29,7 @@ describe('Emperors API', function() {
   });
 
   it('Should post an emperor', function(done) {
-    chai.request('localhost:3000')
+    chai.request('localhost:' + port)
       .post('/api/emperor')
       .send({name: 'Julian', birth: 100, death: 100})
       .end(function(err, res) {
@@ -40,11 +40,21 @@ describe('Emperors API', function() {
   });
 
   it('Should GET list of all emperors', function(done) {
-    chai.request('localhost:3000')
+    chai.request('localhost:' + port)
       .get('/api/emperor')
       .end(function(err, res) {
         expect(err).to.eql(null);
         expect(Array.isArray(res.body)).to.eql(true);
+        done();
+      });
+  });
+
+  it('Should get a single emperor by name', function(done) {
+    chai.request('localhost:' + port)
+      .get('/api/emperor/' + 'Julian')
+      .end(function(err, res) {
+        expect(err).to.eql(null);
+        expect(res.body.length).to.eql(1);
         done();
       });
   });
@@ -62,7 +72,7 @@ describe('Emperors API', function() {
 
     it('Should update existing emperor', function(done) {
       var id = this.testEmperor._id;
-      chai.request('localhost:3000')
+      chai.request('localhost:' + port)
         .put('/api/emperor/' + id )
         .send({title: 'Updated emperor'})
         .end(function(err, res) {
@@ -74,7 +84,7 @@ describe('Emperors API', function() {
 
     it('Should delete existing emperor', function() {
       var id = this.testEmperor._id;
-      chai.request('localhost:3000')
+      chai.request('localhost:' + port)
         .del('/api/emperor/' + id )
         .end(function(err, res) {
           expect(err).to.eql(null);
