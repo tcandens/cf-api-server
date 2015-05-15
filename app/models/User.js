a74efe6d0d7ac96a4f04a4e60666d02a565c5d7a
tcandens <joey.thies@gmail.com>
2015-05-15 14:07:50 -0700

    Working on EAT Switching to new branch to try attaching User methods to prototype

--- app/models/User.js
+++ app/models/User.js
@@ -6,6 +6,7 @@ var DBPASS = process.env.DBPASS || 'kikapoo';
 
 var Sql = require('sequelize');
 var bcrypt = require('bcrypt');
+var eat = require('eat');
 
 var sql = new Sql( DB, DBUSER, DBPASS, { dialect: 'postgres' })
 
@@ -27,6 +28,9 @@ User.methods = {
     bcrypt.compare( password, hash, function( err, res ) {
       callback( err, res );
     });
+  },
+  generateToken: function( id, secret, callback ) {
+    eat.encode({ id: id }, secret, callback );
   }
 }
 
--- app/test.js
+++ app/test.js
@@ -1,24 +1,10 @@
 var User = require('./models/User');
 
-User.methods.generateHash( 'dicks', function( err, hash ) {
-  User.create({ username: 'Toddd', email: 'toddd@toddd.com', password: hash })
-    .then(function( user ) {
-      console.log( user.getDataValue( 'password' ));
-    })
-})
-
 User.findOne({
   where: {
     username: 'Toddd'
   }
 })
   .then(function( user ) {
-    User.methods.checkPassword( 'dicks', user.getDataValue('password'), function( err, res ) {
-      if ( res ) {
-        console.log( 'Password matched' );
-      } else {
-        console.log( 'Wrong password' );
-      }
-    })
+    console.log( user );
   })
-  .catch( console.log )
