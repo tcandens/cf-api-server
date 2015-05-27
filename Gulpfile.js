17f1502a2ddc88ad1a746c987cc9efffeb6dc0e5
tcandens <joey.thies@gmail.com>
2015-05-27 09:30:52 -0700

    Pumping up Gulpfile w webpack, clean, stylus, etc

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,11 +3,41 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-mocha');
+var webpack = require('gulp-webpack');
+var del = require('del');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var bs;
+
+gulp.task('clean:build', function() {
+  return del(['build/'], function( err, res ) {
+    if ( err ) console.log( err );
+  });
+});
+
+gulp.task('webpack:client', function() {
+  return gulp.src('app/public/js/index.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('build/public/js/'));
+});
 
 gulp.task('lint', function() {
   gulp.src(['./lib/**/*.js', './Gruntfile.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('serve:build', function() {
+  bs = browserSync({
+    port: 3434,
+    server: {
+      baseDir: 'build'
+    }
+  });
 });
 
 gulp.task('test', function() {
@@ -26,3 +56,4 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['lint', 'test']);
+gulp.task('serve', ['lint', 'test', ])
